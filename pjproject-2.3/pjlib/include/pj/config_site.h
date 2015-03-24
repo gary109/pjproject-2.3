7d17842414278b78aa077d0d1f0c3ce2acc8325f
gary109 <gary109@gmail.com>
2015-03-24 11:57:49 +0800

    first commit
    built for VS Express 2013

---
+++ pjproject-2.3/pjlib/include/pj/config_site.h
@@ -0,0 +1,8 @@
+//// Uncomment to get minimum footprint (suitable for 1-2 concurrent calls only)
+//#define PJ_CONFIG_MINIMAL_SIZE
+//
+//// Uncomment to get maximum performance
+//#define PJ_CONFIG_MAXIMUM_SPEED
+/* To reduce socket buffers */
+
+#include <pj/config_site_sample.h> 
